10c09a2ecb4b51c47d0c144cb4dabb95586f57fc
mohammedtlemsani <mohammed.tlemsani1-etu@etu.univh2c.ma>
2024-04-30 11:03:24 +0100

    first commit

---
+++ DigitalBanking_backend/src/test/java/ma/enset/digitalbanking_backend/DigitalBankingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.enset.digitalbanking_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalBankingBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
